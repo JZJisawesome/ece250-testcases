48c2e1b2df69fe4fb3423225716a2206615522bd
John Jekel <JZJisawesome123@gmail.com>
2023-01-18 13:13:06 -0500

    More autograder progress

--- jekelautograder.py
+++ jekelautograder.py
@@ -9,24 +9,34 @@
 
 # Imports
 
+import sys
 import os.path
 
 #Functions
 def main():
-    print("Jekel AutoGrader")
+    print("\x1b[95mJekel AutoGrader\x1b[0m")
+    print("\x1b[90mCopyright (c) 2023 John Jekel\x1b[0m\n")
 
     ensure_testcases_are_locatable_or_die()
 
-    die("The autograder isn't quite finished yet", "John is working on it")
+    die("The autograder isn't quite finished yet", "John is working on it :)")
 
 def ensure_testcases_are_locatable_or_die():
-    if not os.path.exists("project"):
+    if not os.path.exists("projects"):
         die("Couldn't locate any testcases", "Are you running the script from the checked-out repository directory?")
 
+#TODO function to get path to all testcases (input and output)
+
+#TODO function to run testcases and collect results
+
+#TODO function to summarize and grade
+
 def die(error_string, tip):
-    print("Shoot, an error occured: " + error_string)
-    print("Maybe this tip will help: " + tip)
-    print("From the ashes of disaster grow the roses of success!")
+    print("\x1b[90m\n---------- snip snip ----------\x1b[0m")
+    print("\x1b[91;1mShoot, an error occured: \x1b[0m\x1b[91;4m" + error_string + "\x1b[0m")
+    print("Maybe this tip will help: \x1b[92m" + tip + "\x1b[0m")
+    print("\x1b[95mFrom the ashes of disaster grow the roses of success!\x1b[0m")
+    sys.exit(1)
 
 #On script entry, call main()
 
